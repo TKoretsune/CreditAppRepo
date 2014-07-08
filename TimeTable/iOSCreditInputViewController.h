9e374c412583e0a779716600f386ca51b72a682a
gucci-megane <gucci.magane.tks@gmail.com>
2014-07-08 13:41:41 +0900

    add gucci’s file

---
+++ TimeTable/iOSCreditInputViewController.h
@@ -0,0 +1,13 @@
+//
+//  iOSCreditInputViewController.h
+//  TimeTable
+//
+//  Created by gucci on 2014/07/08.
+//  Copyright (c) 2014年 是常 雄大. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface iOSCreditInputViewController : UIViewController
+
+@end
---
+++ TimeTable/iOSCurriculumRegistViewController.m
@@ -0,0 +1,13 @@
+//
+//  iOSCurriculumRegistViewController.m
+//  TimeTable
+//
+//  Created by gucci on 2014/07/08.
+//  Copyright (c) 2014年 是常 雄大. All rights reserved.
+//
+
+#import "iOSCurriculumRegistViewController.h"
+
+@implementation iOSCurriculumRegistViewController
+
+@end
